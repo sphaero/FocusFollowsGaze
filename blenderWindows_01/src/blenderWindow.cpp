b3af0c96f59cf6a6fe880010580ff9faef05668d
Arnaud Loonstra <arnaud@sphaero.org>
2013-10-31 16:24:48 +0100

    added blenderWindow class skeleton

---
+++ blenderWindows_01/src/blenderWindow.cpp
@@ -0,0 +1,18 @@
+/*
+ * blenderWindow.cpp
+ *
+ *  Created on: Oct 31, 2013
+ *      Author: arnaud
+ */
+
+#include "blenderWindow.h"
+
+blenderWindow::blenderWindow() {
+	// TODO Auto-generated constructor stub
+
+}
+
+blenderWindow::~blenderWindow() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ blenderWindows_01/src/blenderWindow.h
@@ -0,0 +1,27 @@
+/*
+ * blenderWindow.h
+ *
+ *  Created on: Oct 31, 2013
+ *      Author: arnaud
+ */
+
+#ifndef BLENDERWINDOW_H_
+#define BLENDERWINDOW_H_
+
+#include "ofxMSAInteractiveObject.h"
+
+class blenderWindow: public ofxMSAInteractiveObject {
+public:
+	blenderWindow();
+	virtual ~blenderWindow();
+
+	// see windowState diagrams
+	bool cmdActive;
+	bool active;
+	bool coloredActive;
+
+private:
+	void processColored();
+};
+
+#endif /* BLENDERWINDOW_H_ */
