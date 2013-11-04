0b6f0c738082fd09b33782bda05b67a352dff6da
SabrinaVerhage <sabrinaverhage@gmail.com>
2013-11-04 13:49:20 +0100

    added TDwindow class

---
+++ blenderWindows_01/src/TDWindow.cpp
@@ -0,0 +1,19 @@
+/*
+ *  TDWindow.cpp
+ *  blenderWindows_01
+ *
+ *  Created by Sabrina Verhage on 11/4/13.
+ *  Copyright 2013 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include "TDWindow.h"
+
+
+TDWindow::TDWindow() {
+
+}
+
+TDWindow::~TDWindow() {
+
+}
---
+++ blenderWindows_01/src/TDWindow.h
@@ -0,0 +1,25 @@
+/*
+ *  TDWindow.h
+ *  blenderWindows_01
+ *
+ *  Created by Sabrina Verhage on 11/4/13.
+ *  Copyright 2013 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#ifndef TDWINDOW_H_
+#define TDWINDOW_H_
+
+#include "blenderWindow.h"
+
+class TDWindow: public blenderWindow {
+	
+public:
+	TDWindow();
+	virtual ~TDWindow();
+
+
+
+};
+
+#endif /* TDWINDOW_H_ */
