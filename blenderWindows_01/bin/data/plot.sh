6674fd278913cde904499bb87d142671be4efd40
Arnaud Loonstra <arnaud@iaudy.z25.lan>
2013-11-06 21:00:26 +0100

    ok now with boxes and titles

--- blenderWindows_01/bin/data/plot.sh
+++ blenderWindows_01/bin/data/plot.sh
@@ -10,9 +10,10 @@ gnuplot -p -e "set autoscale;\
  set grid xtics lt 0 lw 1 lc rgb \"#AAAAAA\";\
  set grid ytics lt 0 lw 1 lc rgb \"#AAAAAA\";\
  set style fill solid;\
+ set boxwidth 0.8;\
  set datafile separator \";\";\
  set datafile commentschars \"-\";\
- plot \"< grep PROP 1383763425\" using 2:4 with impulses lc rgb \"green\",
- \"< grep TDWN 1383763425\" using 2:4 with impulses lc rgb \"red\",
- \"< grep TXT1 1383763425\" using 2:4 with impulses lc rgb \"blue\",
- \"< grep TXT2 1383763425\" using 2:4 with impulses lc rgb \"purple\";"
+ plot \"< grep PROP 1383763425\" using 2:4 with boxes lc rgb \"green\" title \"modify property\",
+ \"< grep TDWN 1383763425\" using 2:4 with boxes lc rgb \"red\" title \"rotate cube\",
+ \"< grep TXT1 1383763425\" using 2:4 with boxes lc rgb \"blue\" title \"enter text right\",
+ \"< grep TXT2 1383763425\" using 2:4 with boxes lc rgb \"purple\" title \"enter text bottom\";"
