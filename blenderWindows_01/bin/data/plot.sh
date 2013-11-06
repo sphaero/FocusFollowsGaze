5668e91a8ec26d5afdfb7c42216f23a957b56d24
Arnaud Loonstra <arnaud@iaudy.z25.lan>
2013-11-06 20:54:11 +0100

    yess filtered with impulses

--- blenderWindows_01/bin/data/plot.sh
+++ blenderWindows_01/bin/data/plot.sh
@@ -6,10 +6,13 @@ YLABEL="seconds"
 
 gnuplot -p -e "set autoscale;\
  set xlabel \"$XLABEL\" tc rgb \"black\";\
- set xlabel \"$XLABEL\" tc rgb \"black\";\
+ set ylabel \"$YLABEL\" tc rgb \"black\";\
  set grid xtics lt 0 lw 1 lc rgb \"#AAAAAA\";\
  set grid ytics lt 0 lw 1 lc rgb \"#AAAAAA\";\
  set style fill solid;\
  set datafile separator \";\";\
  set datafile commentschars \"-\";\
- plot \"-\" using 2:4 with boxes"
+ plot \"< grep PROP 1383763425\" using 2:4 with impulses lc rgb \"green\",
+ \"< grep TDWN 1383763425\" using 2:4 with impulses lc rgb \"red\",
+ \"< grep TXT1 1383763425\" using 2:4 with impulses lc rgb \"blue\",
+ \"< grep TXT2 1383763425\" using 2:4 with impulses lc rgb \"purple\";"
