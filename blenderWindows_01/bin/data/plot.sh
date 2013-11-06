4c7ac069d50b5c790eaa14c28c87cb987fd2f913
Arnaud Loonstra <arnaud@iaudy.z25.lan>
2013-11-06 20:27:28 +0100

    with filled boxes

--- blenderWindows_01/bin/data/plot.sh
+++ blenderWindows_01/bin/data/plot.sh
@@ -9,6 +9,7 @@ gnuplot -p -e "set autoscale;\
  set xlabel \"$XLABEL\" tc rgb \"black\";\
  set grid xtics lt 0 lw 1 lc rgb \"#AAAAAA\";\
  set grid ytics lt 0 lw 1 lc rgb \"#AAAAAA\";\
+ set style fill solid;\
  set datafile separator \";\";\
  set datafile commentschars \"-\";\
- plot \"-\" using 2;"
+ plot \"-\" using 2:4 with boxes"
